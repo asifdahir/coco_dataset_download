2b31ae839ce20154ea987fc4f3fbb64527c6f0e9
mac.window.10 <mac.window.10@gmail.com>
2019-12-06 12:33:27 +0500

    first commit

---
+++ download_images.py
@@ -0,0 +1,15 @@
+
+from PythonAPI.pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)
+
+dataDir='/home/home/AndroidStudioProjects/Messaging/visible/coco_dataset_images'
+dataType='val2017'
+annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
+
+# initialize COCO api for instance annotations
+coco=COCO(annFile)
